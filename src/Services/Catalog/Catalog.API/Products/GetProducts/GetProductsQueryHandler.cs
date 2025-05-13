7fbda70721380a866f7bf4e1d5af5ed1fee507f1
lulzsoad <lukasz.lopata.developer@gmail.com>
2025-05-13 21:08:36 +0200

    Added logging behavior

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -9,10 +9,7 @@ namespace Catalog.API.Products.GetProducts
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
-
             var products = await session.Query<Product>().ToListAsync(cancellationToken);
-
             return new GetProductsResult(products);
         }
     }
